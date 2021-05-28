094627c9dcd94fab92d6b296cf08eb755040f007
MarcoSilva0 <marcoantonio02016@outlook.com>
2021-05-28 11:47:17 -0300

    'Começando a arquivar e organizar os arquivos dos cursos'

---
+++ basico/ola.php
@@ -0,0 +1,11 @@
+<div class="titulo">Olá PHP</div>
+
+<?php
+
+//Para imprimir algo na tela do HTML =>
+echo 'Olá ';
+echo 'Mundo!';
+?>
+<!--Outra forma de exibir da qual nn nescessita de echo -->
+<!--Alt+shift+seta para duplicar linha-->
+<?= 'Tudo bem?' ?>
